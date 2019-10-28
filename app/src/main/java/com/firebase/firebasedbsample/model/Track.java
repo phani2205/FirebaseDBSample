19158fbf1f1cd35943f99c9ef47008c461bc3515
phani.2205 <phani.2205@gmail.com>
2019-10-28 20:01:45 +0530

    Firebase DB Update,Delete and retrieve complete guide

---
+++ app/src/main/java/com/firebase/firebasedbsample/model/Track.java
@@ -0,0 +1,28 @@
+package com.firebase.firebasedbsample.model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class Track {
+    private String id;
+    private String trackName;
+    private int rating;
+
+    public Track() {
+
+    }
+
+    public Track(String id, String trackName, int rating) {
+        this.trackName = trackName;
+        this.rating = rating;
+        this.id = id;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+}
